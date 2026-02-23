3699f2e3d8318e96b943bef4bcfb56174efed486
LordDregg <oluwatosinodejide121@gmail.com>
2026-02-23 21:48:30 +0100

    feat: Buy and Delist Property implemented. Refactors

--- src/Week 6/Day 1/PropertyManagement.sol
+++ src/Week 6/Day 1/PropertyManagement.sol
@@ -9,98 +9,121 @@ contract PropertyToken is ERC20 {
     }
 }
 
-contract PropertyManagement {
-    // Errors
-    error Property__InvalidPrice();
-    error Property__OnlyThePropertyOwnerCanPerformThisAction();
-    error Property__PropertyListingFailed();
-    error Property__404_NotFound();
+ contract PropertyManagement {
+        // Errors
+        error Property__InvalidPrice();
+        error Property__OnlyThePropertyOwnerCanPerformThisAction();
+        error Property__PropertyListingFailed();
+        error Property__404_NotFound();
+        error Property__PurchaseFailed();
+        error Property__ThisPropertyIsNotListedForSale();
 
-    // State Variables
-    uint constant public PROPERTY_LISTING_PRICE = 1000;
-    PropertyToken immutable token;
-    
-    struct Property {
-        uint id;
-        string name;
-        uint price;
-        bool isListedForSale;
-        address owner;
-    }
-    
-    Property[] private allProperties;
-    mapping(uint propertyId => Property property) private idToProperty;
+        // Events
+        event PropertyListed(address indexed owner, uint indexed propertyIndex, uint timestamp);
+        event PropertyDelisted(address indexed owner, uint indexed propertyIndex, uint timestamp);
+        event PropertyPurchased(address indexed seller, address indexed buyer, uint indexed propertyId, uint propertyPrice);
 
-    // Modifier
-    modifier onlyPropertyOwner(uint _id) {
-        if (idToProperty[_id].owner != msg.sender) revert Property__OnlyThePropertyOwnerCanPerformThisAction();
-        _;
-    }
-
-    // Functions
-    constructor(address tokenAddress) {
-        token = PropertyToken(tokenAddress);
-    }
-
-    function createNewProperty(string memory _name, uint256 _price) public {
-        if (_price == 0) revert Property__InvalidPrice();
+        // State Variables
+        uint constant public PROPERTY_LISTING_PRICE = 1000;
+        PropertyToken immutable token;
         
-        bool success = token.transferFrom(msg.sender, address(this), PROPERTY_LISTING_PRICE);
-        if(!success) revert Property__PropertyListingFailed();
+        struct Property {
+            uint id;
+            string name;
+            uint price;
+            bool isListedForSale;
+            address owner;
+        }
+        
+        Property[] private allProperties;
+        mapping(uint propertyId => Property property) private idToProperty;
 
-        uint256 _id = block.timestamp;
-
-        Property memory newProperty = Property({
-            id: _id,
-            name: _name,
-            price: _price,
-            isListedForSale: false,
-            owner: msg.sender
-        });
-
-        allProperties.push(newProperty);
-        idToProperty[_id] = newProperty;
-    }
-
-    function removeProperty(uint _id) public onlyPropertyOwner(_id) {
-        bool found;
-
-        for(uint index; index < allProperties.length; index++) {
-            if (allProperties[index].id == _id) {
-                allProperties[index] = allProperties[allProperties.length - 1];
-                allProperties.pop();
-
-                idToProperty[_id] = Property(0, "", 0, false, address(0));
-
-                found = true;
-            }
+        // Modifier
+        modifier onlyPropertyOwner(uint _id) {
+            if (idToProperty[_id].owner != msg.sender) revert Property__OnlyThePropertyOwnerCanPerformThisAction();
+            _;
         }
 
-        if(!found) revert Property__404_NotFound();
-    }
+        // Functions
+        constructor(address tokenAddress) {
+            token = PropertyToken(tokenAddress);
+        }
 
-    function setPropertyForSale(uint _id, bool forSale) public onlyPropertyOwner(_id) {
-        Property storage property = idToProperty[_id];
-        property.isListedForSale = forSale;
-        allProperties[getPropertyIndex(_id)] = property;
-    }
+        function listNewProperty(string memory _name, uint256 _price) public {
+            if (_price == 0) revert Property__InvalidPrice();
+            
+            bool success = token.transferFrom(msg.sender, address(this), PROPERTY_LISTING_PRICE);
+            if(!success) revert Property__PropertyListingFailed();
 
-    //=========================
-    //    Getter Functions
-    //=========================
-    function getPropertyById(uint _id) external view returns (Property memory) {
-        return idToProperty[_id];
-    }
+            uint256 _id = block.timestamp;
 
-    function getAllProperties() external view returns (Property[] memory) {
-        return allProperties;
-    }
+            Property memory newProperty = Property({
+                id: _id,
+                name: _name,
+                price: _price,
+                isListedForSale: false,
+                owner: msg.sender
+            });
 
-    function getPropertyIndex(uint _id) public view returns(uint propertyIndex) {
-        for(uint index; index < allProperties.length; index++) {
-            if (allProperties[index].id == _id) {
-                propertyIndex = index;
+            allProperties.push(newProperty);
+            idToProperty[_id] = newProperty;
+
+            emit PropertyListed(newProperty.owner, newProperty.id, block.timestamp);
+        }
+
+        function delistProperty(uint _id) public onlyPropertyOwner(_id) {
+            uint propertyIndex = getPropertyIndex(_id);
+
+            Property storage property = allProperties[propertyIndex];
+
+            if(property.owner == address(0)) revert Property__404_NotFound();
+
+            allProperties[propertyIndex] = allProperties[allProperties.length - 1];
+            allProperties.pop();
+
+            idToProperty[_id] = Property(0, "", 0, false, address(0));
+
+            emit PropertyDelisted(msg.sender, propertyIndex, block.timestamp);
+        }
+
+        function setPropertyForSale(uint _id, bool forSale) public onlyPropertyOwner(_id) {
+            Property storage property = idToProperty[_id];
+            property.isListedForSale = forSale;
+            allProperties[getPropertyIndex(_id)] = property;
+        }
+
+        function buyProperty(uint _id) public {
+            Property storage property = allProperties[getPropertyIndex(_id)];
+            if (!property.isListedForSale) revert Property__ThisPropertyIsNotListedForSale();
+
+            address initialOwner = property.owner;
+            bool success = token.transferFrom(msg.sender, property.owner, property.price);
+
+            if (!success) revert Property__PurchaseFailed();
+
+            property.owner = msg.sender;
+            property.isListedForSale = false;
+            idToProperty[_id] = property;
+
+            emit PropertyPurchased(initialOwner, msg.sender, property.id, property.price);
+        }
+
+        //=========================
+        //    Getter Functions
+        //=========================
+        function getPropertyById(uint _id) external view returns (Property memory) {
+            return idToProperty[_id];
+        }
+
+        function getAllProperties() external view returns (Property[] memory) {
+            return allProperties;
+        }
+
+        function getPropertyIndex(uint _id) public view returns(uint propertyIndex) {
+            for(uint index; index < allProperties.length; index++) {
+                if (allProperties[index].id == _id) {
+                    propertyIndex = index;
+                }
             }
         }
     }
-}
